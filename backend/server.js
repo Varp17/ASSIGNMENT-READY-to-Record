1387f2f8816fcab139f4b53c3cfc7d41994aea62
Varp17 <varunpatil0217@gmail.com>
2025-10-31 16:04:37 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true, // allow cookies in cross-origin requests
   })
 );
